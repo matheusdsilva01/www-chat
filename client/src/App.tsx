d98cc65c0ae7525fc6b7658591ed2d35c702413d
Matheus Silva <ms25022003@gmail.com>
2022-12-22 15:15:03 -0300

    feat: connection to websocket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,7 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001")
+socket.connect()
 function App() {
   return (
     <div className="App">
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,25 @@
-import { serverHttp } from "./http";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
-const port = 8080;
+const app = express();
+app.use(cors);
 
-serverHttp.listen(port, () => console.log("Server is running"));
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Closed connection", socket.id);
+  });
+});
+serverHttp.listen(3001, () => console.log("Server is running"));
+export { io };
