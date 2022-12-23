ed5c54f9effbf0155652126266b77964d5c0beb4
Matheus Silva <ms25022003@gmail.com>
2022-12-23 11:59:48 -0300

    feat: styled components

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme';
+import GlobalStyles from 'styles/global-styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ client/src/styles/tokens/colors.ts
@@ -0,0 +1,29 @@
+const base = {
+  "black-01": "#212121",
+  "black-02": "#111111",
+  "white-01": "#ffffff",
+};
+
+const text = {
+  "text-contrast-high": "#171717",
+  "text-contrast-low": "#6f6f6f",
+  "text-black": "#000000",
+  "text-white": "#ffffff",
+};
+
+const primary = {
+  primary: "#211F72",
+};
+
+const secondary = {
+  secondary: "#020060",
+};
+
+const colors = {
+  ...base,
+  ...primary,
+  ...secondary,
+  ...text,
+};
+
+export { colors };
